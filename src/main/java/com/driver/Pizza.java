0ac707f2b93642ddec84b1b429eb77a0497ca66a
AmeyPank <pankantiameya@gmail.com>
2023-02-02 11:19:56 +0530

    Pizza bill generator Added

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -6,29 +6,75 @@ public class Pizza {
     private Boolean isVeg;
     private String bill;
 
+    private int extraCheesePrice = 80;
+
+    private int extraToppingsPrice = 70;
+
+    private int backPackPrice = 20;
+    private int basePizzaPrice;
+
+    private boolean isExtraCheeseAdded = false;
+    private boolean isExtraToppingsAdded = false;
+    private boolean isOptedForTakeAway= false;
+
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
         // your code goes here
+        this.isVeg = isVeg;
+        if(this.isVeg){
+            this.price = 300;
+        }else{
+            this.price = 400;
+            this.extraToppingsPrice = 120;
+        }
+        basePizzaPrice = this.price;
     }
 
     public int getPrice(){
+
         return this.price;
     }
 
     public void addExtraCheese(){
         // your code goes here
+        isExtraCheeseAdded = true;
+//        System.out.println("Extra cheese added");
+        this.price += extraCheesePrice;
+
     }
 
     public void addExtraToppings(){
         // your code goes here
+        isExtraToppingsAdded = true;
+//        System.out.println("Extra Toppings added");
+        this.price += extraToppingsPrice;
     }
 
     public void addTakeaway(){
         // your code goes here
+        isOptedForTakeAway = true;
+//        System.out.println("Take Away Opted");
+        this.price += backPackPrice;
     }
 
     public String getBill(){
         // your code goes here
+//        String bill = "";
+        System.out.println("Base Price of the Pizza : "+basePizzaPrice);
+        if(isExtraCheeseAdded){
+            bill += "Extra Cheese Added: "+extraCheesePrice+ "\n";
+
+        }
+        if(isExtraToppingsAdded){
+            bill += "Extra Toppings Added: "+extraToppingsPrice+ "\n";
+
+        }
+        if(isOptedForTakeAway){
+            bill += "Paper Bag Added: "+backPackPrice+ "\n";
+
+        }
+        bill +="Bill: "+this.price + "\n";
+//        System.out.println(bill);
         return this.bill;
     }
 }
